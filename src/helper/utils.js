01d4ba9687106b00482f5cf4b715f6e3641c3de2
Mubasher Mukhtar <mubasherali0331@gmail.com>
2023-04-07 01:40:32 +0500

    added id generator

---
+++ src/helper/utils.js
@@ -0,0 +1,14 @@
+const idGenerator = (idLength = 16, onlyAlphabets = false) => {
+    let chars =
+        "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm[]{}<>:?/!@#%^&*()+0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm[]{}<>:?/!@#%^&*()+";
+    if (onlyAlphabets)
+        chars =
+            "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    let firstChar = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    let id = firstChar[Math.floor(Math.random() * firstChar.length)];
+    for (let i = 0; i < idLength - 1; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        id += chars[randomNumber];
+    }
+    return id;
+};
